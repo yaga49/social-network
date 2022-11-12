009acb7a5a88f46bf9e39a2de96d4140e7310944
yaga49 <komarov.vova2001@mail.ru>
2022-11-12 19:00:32 +0400

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,44 @@
 import React from 'react';
 import './App.css';
-import Header from "./Header";
-import Content from "./Content";
+
 
 
 function App() {
     return (
-        <div>
-            <Header/>
-            <Content/>
+        <div className="app-wrapper">
+            <header className='header'>header</header>
+            <nav className='sidebar'>
+                <div>
+                   <a>profile</a>
+                </div>
+                <div>
+                    <a>setting</a>
+                </div>
+                <div>
+                    <a>music</a>
+                </div>
+                <div>
+                    <a>profile</a>
+                </div>
+                <div>
+                    <a>profile</a>
+                </div>
+            </nav>
+            <div className='content'>
+                <div>
+                    <img src="https://www.slidebackground.com/uploads/rainbow/color-themes-rainbow-desktop-background-.jpg"/>
+                </div>
+                <div>
+                    about
+                </div>
+                <div>
+                    about
+                </div>
+                <div>
+                    about
+                </div>
+            </div>
+
         </div>
     );
 }
